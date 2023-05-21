352d05203877a8c53a5990f60022b1b49f609066
vittao <victorzeni@gmail.com>
2023-05-21 16:33:28 +0000

    Commit inicial, contendo a estrutura serverless e a primeira funcao para inserir item.

---
+++ src/isertItem.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const {v4} = require("uuid")
+const AWS = require("aws-sdk")
+
+const insertItem = async(event) => {
+
+	const {item} = JSON.parse(event.body);
+	const createAt = new Date().toISOString();
+	const id = v4();
+
+	const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+	const newItem = {
+		id,
+		item
+		createdAt,
+		itemStatus: false
+	}
+
+	await dynamoDB.put(
+		{
+			TableName:"ItemTable",
+			Item: newItem
+		}
+	)
+
+	return {
+		statusCode: 200,
+		body: JSON.stringfy(newItem)
+	};
+
+}
+
+module.exports = {
+	handler:insertItem
+}
+
